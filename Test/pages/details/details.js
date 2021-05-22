79ba9075de87d652c9b4226ff1e7b7c7995ef5a4
lixin-ee <61187969+lixin-ee@users.noreply.github.com>
2021-05-22 13:44:14 +0800

    fix bug and add feature by lixin

--- Test/pages/details/details.js
+++ Test/pages/details/details.js
@@ -1,30 +1,84 @@
 // pages/details/details.js
 Page({
-
     /**
      * 页面的初始数据
      */
     data: {
-        problem:
-        {
-            id:1,
-            type:"problem",
-            author:"lisin",
-            content:"That is",
-            title:"zheshitaitou"
+        problem: {
+            id: 1,
+            author: "author",
+            type: "problem",
+            content: "这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content",
+            title: "这是大标题这是大标题这是大标题这是大标题这是大标题这是大标题这是大标题这是大标题这是大标题这是大标题",
+            time: "2021-5-22 10:12",
+            tags: [{
+                name: "标签1"
+            }, {
+                name:"标签哈哈哈"
+            }, {
+                name: "标签2"
+            },{
+                name: "标签5"
+            }, {
+                name:"标哈哈"
+            }, {
+                name: "签前2"
+            },{
+                name: "哈哈"
+            }, {
+                name:"标签哈哈"
+            }, {
+                name: "啦啦啦"
+            }],
+            avatar: "https://img.yzcdn.cn/vant/cat.jpeg",
         },
-        myAnswerList:[
-            {
-                id:1,
-                type:"answer",
-                author:"lisin",
-                content:"That is",
+        myAnswerList: [{
+                id: 1,
+                author: "author",
+                type: "answer",
+                content: "这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content",
+                time: "2021-5-22 10:12",
+                avatar: "https://img.yzcdn.cn/vant/cat.jpeg",
             },
             {
-                id:2,
-                type:"answer",
-                author:"lisin",
-                content:"That is",
+                id: 2,
+                author: "author",
+                type: "answer",
+                content: "这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content",
+                time: "2021-5-22 10:12",
+                avatar: "https://img.yzcdn.cn/vant/cat.jpeg",
+            },
+            {
+                id: 3,
+                author: "author",
+                type: "answer",
+                content: "这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content",
+                time: "2021-5-22 10:12",
+                avatar: "https://img.yzcdn.cn/vant/cat.jpeg",
+            },
+            {
+                id: 4,
+                author: "author",
+                type: "answer",
+                content: "这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content",
+                time: "2021-5-22 10:12",
+                avatar: "https://img.yzcdn.cn/vant/cat.jpeg",
+            },
+            {
+                id: 5,
+                author: "author",
+                type: "answer",
+                content: "这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content",
+                time: "2021-5-22 10:12",
+                avatar: "https://img.yzcdn.cn/vant/cat.jpeg",
+            },
+            {
+                id: 6,
+                author: "author",
+                type: "answer",
+                content: "这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content这是content",
+                time: "2021-5-22 10:12",
+                avatar: "https://img.yzcdn.cn/vant/cat.jpeg",
             }
         ],
     },
@@ -48,11 +102,11 @@ Page({
      */
     onShow: function () {
         if (typeof this.getTabBar === 'function' &&
-        this.getTabBar()) {
-        this.getTabBar().setData({
-          selected: 2
-        })
-    }
+            this.getTabBar()) {
+            this.getTabBar().setData({
+                selected: 2
+            })
+        }
     },
 
     /**
